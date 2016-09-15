5a5bdfcdf3bd2f9c6cfe5504f476ba3fceb6248f
Jack Lightfoot <jack.lightfoot@tee2.org>
2016-09-14 17:25:35 -0700

    added startup notice

--- src/app/MyApps.js
+++ src/app/MyApps.js
@@ -3,9 +3,10 @@ import ReactDOM from 'react-dom';
 import { connect } from 'react-redux';
 import { Link } from 'react-router';
 import {showFlashMessage, removeT2AppFromMyApps, tabChangeIndex} from './actions';
-
+import Paper from 'material-ui/Paper';
 import FloatingActionButton from 'material-ui/FloatingActionButton';
 import ContentAdd from 'material-ui/svg-icons/content/add';
+import RaisedButton from 'material-ui/RaisedButton';
 
 const styles = {
   content: {
@@ -54,6 +55,14 @@ const styles = {
 
   centeredLink: {
 
+  },
+  messageBox: {
+    margin: 20,
+    minHeight: 50,
+    padding: 20,
+    textAlign: 'center',
+    alignSelf: 'center',
+    flex: '0 0 auto'
   }
 };
 
@@ -63,23 +72,31 @@ class MyApps extends Component {
   }
   render () {
     var {appList} = this.props;
+    var pageContent = <Paper style={styles.messageBox} zDepth={2}><div>
+          Your home page is empty! Add some apps using the button below.</div>
+
+          <Link to="/catalog"><RaisedButton style={{margin: 10}} label="Get Started" primary={true} /></Link>
+    </Paper>;
+    if (appList.length > 0) {
+      pageContent = appList.map((tile, i) => (
+                        <div key={i + 1} style={styles.appContainer}>
+                          <div>
+                            <a href={tile.url} target='_blank'>
+                            <img style={styles.appImage} src={tile.img} />
+                            </a>
+                          </div>
+                          <div>
+                            <span>{tile.title}</span>
+                          </div>
+                        </div>
+                    ));
+    }
 
     return (
       <div style={styles.content}>
         <div style={styles.body}>
 
-            {appList.map((tile, i) => (
-                <div key={i + 1} style={styles.appContainer}>
-                  <div>
-                    <a href={tile.url} target='_blank'>
-                    <img style={styles.appImage} src={tile.img} />
-                    </a>
-                  </div>
-                  <div>
-                    <span>{tile.title}</span>
-                  </div>
-                </div>
-            ))}
+        {pageContent}
 
         </div>
         <div style={styles.appActionContainer}>
